f63ff91a0d9dc97928c40d012a94fc9d12cb4bd9
Visaal Patel <visaal.patel@gmail.com>
2020-07-24 19:16:45 +0100

    add animation for field lists

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let fieldValueList = document.getElementsByClassName("filter-value-list")[0];
+let applyAnimation = document.getElementById("animateFilterList");
+applyAnimation.addEventListener("click", () => {
+  fieldValueList.classList.toggle("expanded");
+});
